624b0c280ac80fb84c1ed8bf7cc690adbc99bd05
Job Oboth <trappsavage601@gmail.com>
2026-03-07 16:15:27 +0300

    .

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const express = require("express")
 const expressLayouts = require("express-ejs-layouts");
 const env = require("dotenv").config()
 const app = express()
+const path = require("path")
 const static = require("./routes/static")
 
 
@@ -17,6 +18,7 @@ const static = require("./routes/static")
  *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
+app.set("views", path.join(__dirname, "views"))
 app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************

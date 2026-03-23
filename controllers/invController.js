4006000b406e90181816a9e68c006306c4c13ad2
Job Oboth <trappsavage601@gmail.com>
2026-03-23 23:55:36 +0300

    .

--- controllers/invController.js
+++ controllers/invController.js
@@ -1,5 +1,5 @@
-const invModel = require("../models/inventory-models")
-const utilities = require("..utilities/")
+const invModel = require("../models/inventory-model")
+const utilities = require("../utilities/")
 
 const invCont = {}
 
@@ -9,7 +9,7 @@ const invCont = {}
 invCont.buildByClassificationId = async function (req, res, next) {
     const classification_id = req.params.classificationId
     const data = await invModel.getInventoryByClassificationId(classification_id)
-    const grid = await utilities.buildClassification(data)
+    const grid = await utilities.buildClassificationGrid(data)
     let nav = await utilities.getNav()
     const className = data[0].classification_name
     res.render("./inventory/classification", {
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,8 +1,8 @@
 // Needed resources
 const express = require("express")
 const router = new express.Router()
-const invController = require("../controller/invController")
+const invController = require("../controllers/invController")
 // Route to build inventory by classification
-router.get("/type/:classificationId", invController.buildByClassification);
+router.get("/type/:classificationId", invController.buildByClassificationId);
 
 module.exports = router;

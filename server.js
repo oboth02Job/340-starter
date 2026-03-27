695d497daab9b21f2ae7018708b751c20d0682d9
Job Oboth <trappsavage601@gmail.com>
2026-03-27 10:30:57 +0300

    .

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const pool = require("./database/")
 /* ***********************
  * Middleware
  * ************************/
-app.set("trust proxy", 1);
+
 app.use(
   session({
     store: new (require("connect-pg-simple")(session))({
